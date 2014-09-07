b60d90113e15d26f2282e8fbb204eef41bffd931
Nick <nmapsy@gmail.com>
2014-09-07 19:39:57 -0400

    src/game.js: first read

--- src/game.js
+++ src/game.js
@@ -1,12 +1,19 @@
 var BLOCK_SIZE = 50;
 
+function makeRead() {
+  var last_block = Crafty.e('2D, DOM, Color, Draggable')
+    .attr({x: 0, y: 0, w: BLOCK_SIZE, h: BLOCK_SIZE})
+    .color('#8A4');
+  for (var i = 1; i < 10; i++) {
+    var this_block = Crafty.e('2D, DOM, Color, Draggable')
+      .attr({x: i*BLOCK_SIZE, y: 0, w: BLOCK_SIZE, h: BLOCK_SIZE})
+      .color('#A48');
+    last_block.attach(this_block);
+    last_block = this_block;
+  }
+}
+
 function Game_start() {
   Crafty.init(1024, 768);
-  var parent = Crafty.e('2D, DOM, Color, Draggable')
-    .attr({x: 0, y: 0, w: BLOCK_SIZE, h: BLOCK_SIZE})
-    .color('#A48');
-  var child = Crafty.e('2D, DOM, Color, Draggable')
-    .attr({x: 50, y: 0, w: BLOCK_SIZE, h: BLOCK_SIZE})
-    .color('#8A4');
-  parent.attach(child);
+  makeRead();
 };
