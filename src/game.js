2544213b615d5a1014f4a59f9077ef11214bd084
Nick <nmapsy@gmail.com>
2014-09-08 12:07:14 -0400

    src/game.js: Make arbitrary sequence into a read

--- src/game.js
+++ src/game.js
@@ -1,15 +1,15 @@
-var BLOCK_SIZE = 50;
+var BASE_SIZE = 50;
+var COLORS = {'A':'#A48', 'C':'#8A4', 'G':'#48A', 'T':'#804'};
 
-function makeRead() {
-  var bases = 10;
+function makeRead(seq) {
   var read = Crafty.e('2D, DOM, Color, Draggable')
-    .attr({x: 0, y: 0, w: bases*BLOCK_SIZE, h: BLOCK_SIZE});
+    .attr({x: 0, y: 0, w: seq.length*BASE_SIZE, h: BASE_SIZE});
   read.alpha = 0.0;
   read.z = 10;
-  for (var i = 1; i < bases; i++) {
+  for (var i = 0; i < seq.length; i++) {
     var base = Crafty.e('2D, DOM, Color, Draggable')
-      .attr({x: i*BLOCK_SIZE, y: 0, w: BLOCK_SIZE, h: BLOCK_SIZE})
-      .color('#A48')
+      .attr({x: i*BASE_SIZE, y: 0, w: BASE_SIZE, h: BASE_SIZE})
+      .color(COLORS[seq[i]])
     base.z = 5;
     read.attach(base);
   }
@@ -17,5 +17,5 @@ function makeRead() {
 
 function Game_start() {
   Crafty.init(1024, 768);
-  makeRead();
+  makeRead('GATTACA');
 };
