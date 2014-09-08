bb2e46b2e9700b104defd65b6f29b7437a3b536e
Nick <nmapsy@gmail.com>
2014-09-08 17:50:30 -0400

    src/game.js: makeRead(): specify where to make the read

--- src/game.js
+++ src/game.js
@@ -1,16 +1,16 @@
 var BASE_SIZE = 50;
 var COLORS = {'A':'#A44', 'C':'#448', 'G':'#484', 'T':'#AA4'};
 
-function makeRead(seq) {
+function makeRead(seq, x, y) {
   // Make an invisible entity the size of the entire read, which will
   // be what the user actually clicks and drags. The bases will be
   // behind it and attached to it.
   var read = Crafty.e('Read')
-    .attr({x: 0, y: 0, w: seq.length*BASE_SIZE, h: BASE_SIZE});
+    .attr({x: x, y: y, w: seq.length*BASE_SIZE, h: BASE_SIZE});
   // Make each base in the sequence, attach to the read
   for (var i = 0; i < seq.length; i++) {
     var base = Crafty.e('Base')
-      .attr({x: i*BASE_SIZE, y: 0, w: BASE_SIZE, h: BASE_SIZE})
+      .attr({x: x+i*BASE_SIZE, y: y, w: BASE_SIZE, h: BASE_SIZE})
       .color(COLORS[seq[i]])
       .text(seq[i]);
     read.attach(base); // makes the base move when the read moves
@@ -37,5 +37,6 @@ function snap(coordinate) {
 
 function Game_start() {
   Crafty.init(1024, 768);
-  makeRead('GATTACA');
+  makeRead('GATTACA', 0, 0);
+  makeRead('TACAGAT', 0, 100);
 };
