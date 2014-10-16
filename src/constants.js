5945153b5463e94af2dfec23764a0fa88f5c8ab1
Nick <nmapsy@gmail.com>
2014-10-16 14:12:14 -0400

    src/{game,constants}.js: start reads in BANK, shorten MAIN.

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ var NUM_READS = 7;
 // x and y set where the top-left corner of the panel are.
 var HEAD = {width: 1000, height: 50, x: 0, y: 0};
 var CONSENSUS = {width: 1000, height: 100, x: 0, y: HEAD.y+HEAD.height};
-var MAIN = {width: 1000, height: 500, x: 0, y: CONSENSUS.y+CONSENSUS.height};
+var MAIN = {width: 1000, height: 400, x: 0, y: CONSENSUS.y+CONSENSUS.height};
 var BANK = {width: 1000, height: NUM_READS*BASE_SIZE, x: 0, y: MAIN.y+MAIN.height};
 // Size of entire Crafty game area
 var GAME = {width: 1001, height: 1+HEAD.height+CONSENSUS.height+MAIN.height+BANK.height};
--- src/game.js
+++ src/game.js
@@ -50,7 +50,7 @@ function newGame(reference, seed) {
   console.log("Shhh, the answer is "+Game.reference+"!");
   var reads = wgsim(Game.reference, NUM_READS, READ_LENGTH, 1);
   for (var i = 0; i < reads.length; i++) {
-    makeRead(reads[i], MAIN.x+i*BASE_SIZE, MAIN.y+i*BASE_SIZE);
+    makeRead(reads[i], MAIN.x+i*BASE_SIZE, BANK.y+i*BASE_SIZE);
   }
   Game.baseGrid.update();
   calcConsensus(Game.baseGrid);
